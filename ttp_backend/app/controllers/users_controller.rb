b3e537fbbc821c9c79b539e0db237f6308b494e0
github username <justinzhao265@gmail.com>
2020-03-09 17:02:26 -0400

    final

--- ttp_backend/app/controllers/users_controller.rb
+++ ttp_backend/app/controllers/users_controller.rb
@@ -6,8 +6,8 @@ class UsersController < ApplicationController
   def create
     user = User.new(userParams)
     if user.save
-        token = encode_token(user.id)
-        render json: {user: user, token: token}
+        # token = encode_token(user.id)
+        render json: {user: user}
     else
         render json: {errors: user.errors.full_messages}, status: :not_acceptable
     end
