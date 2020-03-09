6bc0b033ad9de41bda1e2eecb2b67b7c0e255140
github username <justinzhao265@gmail.com>
2020-03-09 13:52:35 -0400

    init

--- ttp_backend/db/schema.rb
+++ ttp_backend/db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_03_05_173633) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "transactions", force: :cascade do |t|
     t.string "ticker"
     t.integer "shares"
